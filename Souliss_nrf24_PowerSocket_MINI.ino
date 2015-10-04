8a94ca72d677f6433aad96a3e0bd1f66a5691c6a
Marcolino <marco.pozzuolo@gmail.com>
2015-10-04 22:52:08 +0200

    Modifiche per supportare l'ultima versione di friariello

--- Souliss_nrf24_PowerSocket_MINI.ino
+++ Souliss_nrf24_PowerSocket_MINI.ino
@@ -72,7 +72,7 @@ void loop(){
 		UPDATEFAST();
 		FAST_50ms() {	// We process the logic and relevant input and output every 50 milliseconds
 			//Checking Pushbutton
-			Souliss_DigIn(PIN_BUTTON, Souliss_T1n_ToogleCmd, memory_map, POWER_SOCKET);
+			Souliss_DigIn(PIN_BUTTON, Souliss_T1n_ToggleCmd, memory_map, POWER_SOCKET);
 			//Writing relè status
 			Souliss_DigOut(PIN_RELE, Souliss_T1n_Coil, memory_map, POWER_SOCKET);
 			
